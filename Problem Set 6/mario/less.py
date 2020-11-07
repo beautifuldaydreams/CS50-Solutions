94024c359ab940c26868f6799c8ccf7ffd9e1774
Lisa <69561759+beautifuldaydreams@users.noreply.github.com>
2020-11-07 21:52:55 +1300

    Create less.py

---
+++ Problem Set 6/mario/less.py
@@ -0,0 +1,14 @@
+invalid = True
+
+while invalid:
+    try:
+        height = int(input("Height:"))
+    except ValueError:
+        continue
+    else: 
+        if 0 < height < 9:
+            invalid = False
+
+for x in range(height):
+    print(" " * (height - x - 1), end = "")
+    print("#" * (x + 1))
